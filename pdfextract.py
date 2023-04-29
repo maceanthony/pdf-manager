555a0621aad0d4aa0bd197a094dfe951ac81e0c2
maceanthony <maceanthony@gmail.com>
2023-04-28 21:37:59 -0400

    working script to extract pages, does not name pages correctely and potentially does not match all file name types

--- pdfextract.py
+++ pdfextract.py
@@ -1,42 +1,37 @@
 import os
-from PyPDF2 import PdfFileReader, PdfFileWriter
-
-# define the page ranges to extract from the PDF
-page_range1 = (0, 4)  # first 5 pages
-page_range2 = (5, 9)  # next 5 pages
+from PyPDF2 import PdfReader, PdfWriter
 
 # define the root folder where PDF files are located
-root_folder = 'path/to/root/folder'
+root_folder = 'D:\pdf extract test'
 
-# define the specific PDF file name to extract
-pdf_file_name = 'example.pdf'
+# define the page ranges to extract from the PDF
+page_range1 = (17, 20)  # pages 18-20
+page_range2 = (20, 22)  # pages 21-22
 
 # loop through all subfolders and files in the root folder
 for subdir, dirs, files in os.walk(root_folder):
-    # get the last part of the subfolder path as the suffix to add to the PDF names
-    suffix = os.path.basename(os.path.normpath(subdir))
     for file_name in files:
-        # check if the file is a PDF file and has the specified name
-        if file_name.endswith('.pdf') and file_name == pdf_file_name:
+        # check if the file is a PDF file named App.pdf
+        if file_name == 'App.pdf':
             # open the PDF file in read mode
             with open(os.path.join(subdir, file_name), 'rb') as file:
                 # read the PDF file with PyPDF2
-                pdf = PdfFileReader(file)
-                # get the total number of pages in the PDF
-                num_pages = pdf.getNumPages()
-                # create a PdfFileWriter object to write the extracted PDFs
-                writer = PdfFileWriter()
+                pdf = PdfReader(file)
+                # create a PdfWriter object to write the extracted PDFs
+                writer = PdfWriter()
                 # extract the first page range and add to the writer
                 for page_num in range(*page_range1):
-                    writer.addPage(pdf.getPage(page_num))
-                # write the first extracted PDF to disk
-                with open(os.path.join(subdir, f'{file_name.split(".")[0]}_{suffix}_part1.pdf'), 'wb') as output_file:
+                    writer.add_page(pdf.pages[page_num])
+                # get the parent folder name to use in the output file name
+                parent_folder = os.path.basename(os.path.normpath(subdir))
+                # write the first extracted PDF to disk with the folder name as a prefix
+                with open(os.path.join(subdir, f'{parent_folder}_part1.pdf'), 'wb') as output_file:
                     writer.write(output_file)
-                # create a new PdfFileWriter object for the second extracted PDF
-                writer = PdfFileWriter()
+                # create a new PdfWriter object for the second extracted PDF
+                writer = PdfWriter()
                 # extract the second page range and add to the writer
                 for page_num in range(*page_range2):
-                    writer.addPage(pdf.getPage(page_num))
-                # write the second extracted PDF to disk
-                with open(os.path.join(subdir, f'{file_name.split(".")[0]}_{suffix}_part2.pdf'), 'wb') as output_file:
+                    writer.add_page(pdf.pages[page_num])
+                # write the second extracted PDF to disk with the folder name as a prefix
+                with open(os.path.join(subdir, f'{parent_folder}_part2.pdf'), 'wb') as output_file:
                     writer.write(output_file)
