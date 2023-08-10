6350b2b102ffa10d365a981ea7e0de6e6e9c08e1
windu-ant <maceanthony@gmail.com>
2023-08-10 13:05:51 -0400

    changed topdown to False

--- delete ALL empty folders/deleteALLemptyfolders.py
+++ delete ALL empty folders/deleteALLemptyfolders.py
@@ -2,7 +2,8 @@ import os
 
 # define a function to delete empty folders
 def delete_empty_folders(directory):
-    for root, dirs, files in os.walk(directory, topdown=True):
+    # topdown=False will delete subdirs first (empty folder within folder then now empty folder)
+    for root, dirs, files in os.walk(directory, topdown=False):
         for dir in dirs:
             folder_path = os.path.join(root, dir)
             if not os.listdir(folder_path):
